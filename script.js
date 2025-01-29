449e36b6538a407b203dfdb345b56ea4ff2895da
Thehan-MD <veronica1978online@gmail.com>
2025-01-30 00:47:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+function showDetails() {
+    document.getElementById("content").innerHTML = `
+        <h2 class="title">CYBER THEHAN</h2>
+        <p>FULLNAME: THEHAN VINDULA</p>
+        <p>NICKNAME: CYBER THEHAN</p>
+        <p>AGE: 18+</p>
+        <p>ABILITIES:</p>
+        <ul>
+            <li>★ PYTHON, JAVA, NODE JS, HTML LANGUAGES</li>
+            <li>★ GITHUB, NGROK, IP ADDRESS (PVT) WEB DEVELOPMENT</li>
+            <li>★ APK DEVELOPMENT</li>
+            <li>★ WHATSAPP NUMBER BAN, UNBAN AND OFFLINE SERVICE</li>
+            <li>★ BOT DEPLOYMENT</li>
+        </ul>
+        <p>FROM: BADULLA, SRI LANKA</p>
+    `;
+}
+
+function openWhatsApp() {
+    window.open("https://wa.me/94725671018", "_blank");
+}
+
+function showCharges() {
+    document.getElementById("content").innerHTML = `
+        <h2 class="title">CYBER THEHAN</h2>
+        <h3>WEB DEVELOPMENT</h3>
+        <p>★ IP ONLY (PVT): $1</p>
+        <p>★ GITHUB HOST: $3.3</p>
+        <p>★ NGROK HOST: $4</p>
+        <p>★ PRIVATE SERVER HOST: $100</p>
+        
+        <h3>WHATSAPP</h3>
+        <p>★ NUMBER UNBAN</p>
+        <ul>
+            <li>⫸ PERMANENT - $1</li>
+            <li>⫸ TERMINAL - $0.33</li>
+        </ul>
+        <p>★ NUMBER BAN</p>
+        <ul>
+            <li>⫸ PERMANENT - $2</li>
+            <li>⫸ TERMINAL - $0.66</li>
+        </ul>
+        <p>★ NUMBER OFFLINE SERVICE</p>
+        <ul>
+            <li>⫸ PERMANENT - $2</li>
+            <li>⫸ 24 HOURS - $1.33</li>
+            <li>⫸ 6 HOURS - $1</li>
+            <li>⫸ 1 HOUR - $0.66</li>
+        </ul>
+        <p>★ MULTI DEVICE BOT DEPLOY - $1</p>
+        <p>★ BUGBOT DEPLOY - $2</p>
+
+        <h3>APP MODDING</h3>
+        <p>★ ANY APP - $10</p>
+    `;
+}
